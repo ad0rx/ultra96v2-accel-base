45de5e01a523caba14281368e34cf7419379ba63
Brad Whitlock <bradley.whitlock@gmail.com>
2020-04-21 13:09:39 -0600

    put on hold while I generate sysroots scripts

--- bin/gen-vitis-platform.sh
+++ bin/gen-vitis-platform.sh
@@ -16,17 +16,18 @@ then
 fi
 
 # Defined in setup.sh
-#p_delete_existing_dir_then_make ${G_VIVADO_PROJECT_DIR}
+p_delete_existing_dir_then_make ${G_VITIS_PLATFORM_PROJECT_DIR}
 
-# Create output dirs needed by vivado project
-#mkdir -p ${G_PFM_DIR}
+# Create output dirs
+mkdir -p ${G_PFM_DIR}
+mkdir -p ${G_BOOT_DIR}
+mkdir -p ${G_SYSROOTS_DIR}
 
-# Generate new vivado project
-#cd ${G_VIVADO_PROJECT_DIR}
+# Generate new vitis project
+cd ${G_VITIS_PLATFORM_PROJECT_DIR}
 
 # See bin/ultra96v2-accel-base-bd.tcl for details
-#vivado -source ${PWS}/bin/ultra96v2-accel-base-bd.tcl
-#vivado -mode batch -source ${PWS}/bin/ultra96v2-accel-base-bd.tcl
-#vivado -mode tcl -source ${PWS}/bin/ultra96v2-accel-base-bd.tcl
+#xsct ${PWS}/bin/vitis-platform.tcl
+xsct -interactive ${PWS}/bin/vitis-platform.tcl
 
 echo "${ME}: Exit"
