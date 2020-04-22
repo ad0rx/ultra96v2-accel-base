deb4dd325694db15fd307de10f175bfb1e9e9ed9
Brad Whitlock <bradley.whitlock@gmail.com>
2020-04-22 16:43:14 -0600

    incremental save, not quite working

--- bin/gen-petalinux-project.sh
+++ bin/gen-petalinux-project.sh
@@ -17,18 +17,26 @@ then
 fi
 
 # Defined in setup.sh
-#p_delete_existing_dir_then_make ${G_VITIS_PLATFORM_PROJECT_DIR}
+p_delete_existing_dir_then_make ${G_PETALINUX_PROJECT_DIR}
 
 # Create output dirs
-#mkdir -p ${G_PFM_DIR}
-#mkdir -p ${G_BOOT_DIR}
-#mkdir -p ${G_SYSROOTS_DIR}
+mkdir -p ${G_PFM_DIR}
+mkdir -p ${G_BOOT_DIR}
+mkdir -p ${G_SYSROOTS_DIR}
 
 # Generate new vitis project
-#cd ${G_VITIS_PLATFORM_PROJECT_DIR}
+cd ${G_PETALINUX_PROJECT_DIR}
 
-# See bin/ultra96v2-accel-base-bd.tcl for details
-#xsct ${PWS}/bin/vitis-platform.tcl
-#xsct -interactive ${PWS}/bin/vitis-platform.tcl
+# Create the base petalinux project based on Ultra96v2 BSP
+petalinux-create -t project -n ${PROJ} -s ${G_PETALINUX_BSP_FILE}
+cd ${PROJ}
+
+# Pull in custom hardware
+petalinux-config --get-hw-description=${G_PFM_DIR} --defconfig
+
+# Disable 96boards-sensors due to build failures from bad dependency URL
+p_disable_petalinux_rootfs_packages "packagegroup-petalinux-96boards-sensors"
+
+time petalinux-build
 
 echo "${ME}: Exit"
