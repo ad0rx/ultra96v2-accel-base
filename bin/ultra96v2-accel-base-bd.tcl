89b1260e7b6556f6446b8a897625f9b0ef9d7524
Brad Whitlock <bradley.whitlock@gmail.com>
2020-05-11 16:19:28 -0600

    added 32b GPO connected to VIO for LED like debug capability

--- bin/ultra96v2-accel-base-bd.tcl
+++ bin/ultra96v2-accel-base-bd.tcl
@@ -123,9 +123,11 @@ set bCheckIPsPassed 1
 set bCheckIPs 1
 if { $bCheckIPs == 1 } {
    set list_check_ips "\
+xilinx.com:ip:axi_gpio:2.0\
 xilinx.com:ip:axi_intc:4.1\
 xilinx.com:ip:clk_wiz:6.0\
 xilinx.com:ip:proc_sys_reset:5.0\
+xilinx.com:ip:vio:3.0\
 xilinx.com:ip:zynq_ultra_ps_e:3.3\
 xilinx.com:ip:xlconcat:2.1\
 xilinx.com:ip:xlconstant:1.1\
@@ -254,6 +256,12 @@ proc create_root_design { parentCell } {
 
   # Create ports
 
+  # Create instance: axi_gpio_0, and set properties
+  set axi_gpio_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0 ]
+  set_property -dict [ list \
+   CONFIG.C_ALL_OUTPUTS {1} \
+ ] $axi_gpio_0
+
   # Create instance: axi_intc_0, and set properties
   set axi_intc_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_intc:4.1 axi_intc_0 ]
   set_property -dict [ list \
@@ -311,8 +319,14 @@ proc create_root_design { parentCell } {
   # Create instance: ps8_0_axi_periph, and set properties
   set ps8_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 ps8_0_axi_periph ]
   set_property -dict [ list \
-   CONFIG.NUM_MI {1} \
+   CONFIG.NUM_MI {2} \
  ] $ps8_0_axi_periph
+
+  # Create instance: vio_0, and set properties
+  set vio_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:vio:3.0 vio_0 ]
+  set_property -dict [ list \
+   CONFIG.C_NUM_PROBE_OUT {0} \
+ ] $vio_0
 
   # Create instance: zynq_ultra_ps_e_0, and set properties
   set zynq_ultra_ps_e_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.3 zynq_ultra_ps_e_0 ]
@@ -1030,22 +1044,25 @@ proc create_root_design { parentCell } {
 
   # Create interface connections
   connect_bd_intf_net -intf_net ps8_0_axi_periph_M00_AXI [get_bd_intf_pins axi_intc_0/s_axi] [get_bd_intf_pins ps8_0_axi_periph/M00_AXI]
+  connect_bd_intf_net -intf_net ps8_0_axi_periph_M01_AXI [get_bd_intf_pins axi_gpio_0/S_AXI] [get_bd_intf_pins ps8_0_axi_periph/M01_AXI]
   connect_bd_intf_net -intf_net zynq_ultra_ps_e_0_M_AXI_HPM0_LPD [get_bd_intf_pins ps8_0_axi_periph/S00_AXI] [get_bd_intf_pins zynq_ultra_ps_e_0/M_AXI_HPM0_LPD]
 
   # Create port connections
   connect_bd_net -net axi_intc_0_irq [get_bd_pins axi_intc_0/irq] [get_bd_pins zynq_ultra_ps_e_0/pl_ps_irq0]
   connect_bd_net -net clk_wiz_0_clk_out1 [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins proc_sys_reset_0/slowest_sync_clk]
   connect_bd_net -net clk_wiz_0_clk_out2 [get_bd_pins clk_wiz_0/clk_out2] [get_bd_pins proc_sys_reset_1/slowest_sync_clk]
-  connect_bd_net -net clk_wiz_0_clk_out3 [get_bd_pins axi_intc_0/s_axi_aclk] [get_bd_pins clk_wiz_0/clk_out3] [get_bd_pins proc_sys_reset_2/slowest_sync_clk] [get_bd_pins ps8_0_axi_periph/ACLK] [get_bd_pins ps8_0_axi_periph/M00_ACLK] [get_bd_pins ps8_0_axi_periph/S00_ACLK] [get_bd_pins zynq_ultra_ps_e_0/maxihpm0_lpd_aclk]
+  connect_bd_net -net clk_wiz_0_clk_out3 [get_bd_pins axi_gpio_0/s_axi_aclk] [get_bd_pins axi_intc_0/s_axi_aclk] [get_bd_pins clk_wiz_0/clk_out3] [get_bd_pins proc_sys_reset_2/slowest_sync_clk] [get_bd_pins ps8_0_axi_periph/ACLK] [get_bd_pins ps8_0_axi_periph/M00_ACLK] [get_bd_pins ps8_0_axi_periph/M01_ACLK] [get_bd_pins ps8_0_axi_periph/S00_ACLK] [get_bd_pins vio_0/clk] [get_bd_pins zynq_ultra_ps_e_0/maxihpm0_lpd_aclk]
   connect_bd_net -net clk_wiz_0_clk_out4 [get_bd_pins clk_wiz_0/clk_out4] [get_bd_pins proc_sys_reset_3/slowest_sync_clk]
   connect_bd_net -net clk_wiz_0_clk_out5 [get_bd_pins clk_wiz_0/clk_out5] [get_bd_pins proc_sys_reset_4/slowest_sync_clk]
   connect_bd_net -net clk_wiz_0_locked [get_bd_pins clk_wiz_0/locked] [get_bd_pins proc_sys_reset_0/dcm_locked] [get_bd_pins proc_sys_reset_1/dcm_locked] [get_bd_pins proc_sys_reset_2/dcm_locked] [get_bd_pins proc_sys_reset_3/dcm_locked] [get_bd_pins proc_sys_reset_4/dcm_locked]
+  connect_bd_net -net gpio_0_o [get_bd_pins axi_gpio_0/gpio_io_o] [get_bd_pins vio_0/probe_in0]
   connect_bd_net -net interrupt_concat_dout [get_bd_pins axi_intc_0/intr] [get_bd_pins interrupt_concat/dout]
-  connect_bd_net -net proc_sys_reset_2_peripheral_aresetn [get_bd_pins axi_intc_0/s_axi_aresetn] [get_bd_pins proc_sys_reset_2/peripheral_aresetn] [get_bd_pins ps8_0_axi_periph/ARESETN] [get_bd_pins ps8_0_axi_periph/M00_ARESETN] [get_bd_pins ps8_0_axi_periph/S00_ARESETN]
+  connect_bd_net -net proc_sys_reset_2_peripheral_aresetn [get_bd_pins axi_gpio_0/s_axi_aresetn] [get_bd_pins axi_intc_0/s_axi_aresetn] [get_bd_pins proc_sys_reset_2/peripheral_aresetn] [get_bd_pins ps8_0_axi_periph/ARESETN] [get_bd_pins ps8_0_axi_periph/M00_ARESETN] [get_bd_pins ps8_0_axi_periph/M01_ARESETN] [get_bd_pins ps8_0_axi_periph/S00_ARESETN]
   connect_bd_net -net zynq_ultra_ps_e_0_pl_clk0 [get_bd_pins clk_wiz_0/clk_in1] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
   connect_bd_net -net zynq_ultra_ps_e_0_pl_resetn0 [get_bd_pins clk_wiz_0/resetn] [get_bd_pins proc_sys_reset_0/ext_reset_in] [get_bd_pins proc_sys_reset_1/ext_reset_in] [get_bd_pins proc_sys_reset_2/ext_reset_in] [get_bd_pins proc_sys_reset_3/ext_reset_in] [get_bd_pins proc_sys_reset_4/ext_reset_in] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
 
   # Create address segments
+  assign_bd_address -offset 0x80010000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_ultra_ps_e_0/Data] [get_bd_addr_segs axi_gpio_0/S_AXI/Reg] -force
   assign_bd_address -offset 0x80000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_ultra_ps_e_0/Data] [get_bd_addr_segs axi_intc_0/S_AXI/Reg] -force
 
 
@@ -1053,6 +1070,7 @@ proc create_root_design { parentCell } {
   current_bd_instance $oldCurInst
 
   # Create PFM attributes
+  set_property PFM_NAME {avnet:ultra96v2:ultra96v2-accel-base:1.0} [get_files [current_bd_design].bd]
   set_property PFM.CLOCK {clk_out1 {id "1" is_default "false" proc_sys_reset "/proc_sys_reset_0" status "fixed"} clk_out2 {id "2" is_default "false" proc_sys_reset "/proc_sys_reset_1" status "fixed"} clk_out3 {id "0" is_default "true" proc_sys_reset "/proc_sys_reset_2" status "fixed"} clk_out4 {id "4" is_default "false" proc_sys_reset "/proc_sys_reset_3" status "fixed"} clk_out5 {id "5" is_default "false" proc_sys_reset "/proc_sys_reset_4" status "fixed"}} [get_bd_cells /clk_wiz_0]
   set_property PFM.AXI_PORT {M_AXI_HPM0_FPD {memport "M_AXI_GP" sptag "" memory ""} M_AXI_HPM1_FPD {memport "M_AXI_GP" sptag "" memory ""} S_AXI_HPC0_FPD {memport "S_AXI_HPC" sptag "" memory ""} S_AXI_HPC1_FPD {memport "S_AXI_HPC" sptag "" memory ""} S_AXI_HP0_FPD {memport "S_AXI_HP" sptag "" memory ""} S_AXI_HP1_FPD {memport "S_AXI_HP" sptag "" memory ""} S_AXI_HP2_FPD {memport "S_AXI_HP" sptag "" memory ""} S_AXI_HP3_FPD {memport "S_AXI_HP" sptag "" memory ""}} [get_bd_cells /zynq_ultra_ps_e_0]
 
@@ -1069,92 +1087,5 @@ proc create_root_design { parentCell } {
 
 create_root_design ""
 
-# Begin Accel Platform Stuff
-set pws           $::env(PWS)
-set proj          $::env(PROJ)
-set pfm_dir       $::env(G_PFM_DIR)
-set xsa_file_name $::env(G_XSA_FILE_NAME)
-set board_id      "ultra96v2"
-set bd_file       "myproj/project_1.srcs/sources_1/bd/design_1/design_1.bd"
-set wrapper_file  "myproj/project_1.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v"
-set dynamic_postlink_file "${pws}/bin/dynamic_postlink.tcl"
-set maxThreads    $::env(G_NUM_CPU_CORES)
-
-set_param general.maxThreads ${maxThreads}
-
-# Enable platform interfaces
-set_property PFM_NAME "avnet:${board_id}:${proj}:1.0" \
-    [get_files ${bd_file}]
-
-# Mark interfaces for accel use
-# Currently already done above, but would like to do programmatically
-# down here, later
-#set_property PFM.CLOCK
-#{
-# clk_out1 { id "1" is_default "false" proc_sys_reset "/proc_sys_reset_0" status "fixed"}
-# clk_out2 { id "2" is_default "false" proc_sys_reset "/proc_sys_reset_1" status "fixed"}
-# clk_out3 { id "0" is_default "true"  proc_sys_reset "/proc_sys_reset_2" status "fixed"}
-# clk_out4 { id "4" is_default "false" proc_sys_reset "/proc_sys_reset_3" status "fixed"}
-# clk_out5 { id "5" is_default "false" proc_sys_reset "/proc_sys_reset_4" status "fixed"}
-#} [get_bd_cells /clk_wiz_0]
-
-#set_property PFM.AXI_PORT
-#{
-# M_AXI_HPM0_FPD {memport "M_AXI_GP"  sptag "" memory ""}
-# M_AXI_HPM1_FPD {memport "M_AXI_GP"  sptag "" memory ""}
-# S_AXI_HPC0_FPD {memport "S_AXI_HPC" sptag "" memory ""}
-# S_AXI_HPC1_FPD {memport "S_AXI_HPC" sptag "" memory ""}
-# S_AXI_HP0_FPD  {memport "S_AXI_HP"  sptag "" memory ""}
-# S_AXI_HP1_FPD  {memport "S_AXI_HP"  sptag "" memory ""}
-# S_AXI_HP2_FPD  {memport "S_AXI_HP"  sptag "" memory ""}
-# S_AXI_HP3_FPD  {memport "S_AXI_HP"  sptag "" memory ""}
-#} [get_bd_cells /zynq_ultra_ps_e_0]
-
-
-# UG1393 CH52
-set_property platform.design_intent.embedded true        [current_project]
-set_property platform.design_intent.server_managed false [current_project]
-set_property platform.design_intent.external_host false  [current_project]
-set_property platform.design_intent.datacenter false     [current_project]
-set_property platform.default_output_type "sd_card"      [current_project]
-set_property platform.post_sys_link_tcl_hook ${dynamic_postlink_file} [current_project]
-
-regenerate_bd_layout
-validate_bd_design -force
-save_bd_design
-
-# Trying to do this programmatically so that I can add arbitrary IP
-# later, and still have this portion of the script work
-set l_ips [get_ips -all]
-foreach ip $l_ips {
-    catch { config_ip_cache -export $ip }
-}
-
-export_ip_user_files -of_objects                                      \
-    [get_files ${bd_file}] \
-    -no_script -sync -force -quiet
-
-create_ip_run [get_files -of_objects [get_fileset sources_1]          \
-		   ${bd_file}]
-
-# Apparently, there is no lmap function in Vivado's Tcl...shame
-# Generate a list with members representing strings to match synth jobs
-foreach a $l_ips { lappend l_ips_synth [append a "_synth_1"] }
-
-launch_runs -jobs ${maxThreads} $l_ips_synth
-
-# Try to just wait on the last run, appears to work
-wait_on_run [lindex $l_ips_synth end]
-
-# Create the HDL wrapper top level
-make_wrapper -files [get_files ${bd_file}] -top
-add_files -norecurse ${wrapper_file}
-
-launch_runs synth_1 -jobs ${maxThreads}
-wait_on_run synth_1
-
-launch_runs impl_1 -to_step write_bitstream -jobs ${maxThreads}
-wait_on_run impl_1
-
-write_hw_platform    -include_bit -force ${pfm_dir}/${xsa_file_name}
-validate_hw_platform -verbose            ${pfm_dir}/${xsa_file_name}
+source $::env(PWS)/bin/accel-platform.tcl
+accel-platform
